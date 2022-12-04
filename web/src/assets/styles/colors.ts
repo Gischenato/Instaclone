d5583b1e73d50501f987d13529660ffaf2727f88
Gischenato <gischenato@gmail.com>
2022-12-03 21:04:53 -0300

    colors

---
+++ web/src/assets/styles/colors.ts
@@ -0,0 +1,16 @@
+export default {
+    white: "#ffffff",
+    black: "#000000",
+
+    text: "#000000",
+
+    primary: "#fafafa",
+
+    post_bg: "#ffffff",
+    post_border: "#dbdbdb",
+
+    sidebar_bg: "#ffffff",
+    sidebar_border: "#dbdbdb",
+    sidebar_option_hover: "#dddddd"
+
+}
