8a0f23ee941b7fced43051b0ec1423bcb9ced0b1
Gischenato <gischenato@gmail.com>
2022-12-04 01:53:38 -0300

    gradient

--- web/src/assets/styles/colors.ts
+++ web/src/assets/styles/colors.ts
@@ -11,6 +11,7 @@ export default {
 
     sidebar_bg: "#ffffff",
     sidebar_border: "#dbdbdb",
-    sidebar_option_hover: "#dddddd"
+    sidebar_option_hover: "#dddddd",
 
+    profile_gradient: '222.87deg, #D300C5 14.27%, #FFCA00 85.4%'
 }
