2a0f735168e5c929460bee000eba8d8003eff919
Gischenato <gischenato@gmail.com>
2022-12-04 01:53:49 -0300

    users mocks

---
+++ web/src/assets/mocks/users.ts
@@ -0,0 +1,36 @@
+const users = [
+    {
+        name: 'John Doe',
+        avatar: 'https://picsum.photos/200/300'
+    },
+    {
+      name: 'Leticia',
+      avatar: 'https://picsum.photos/200/307'
+    },
+    {
+      name: 'Jane Doe',
+      avatar: 'https://picsum.photos/200/301'
+    },
+    {
+        name: 'Jane Smith',
+        avatar: 'https://picsum.photos/200/303'
+    },
+    {
+        name: 'Maria Lopez',
+        avatar: 'https://picsum.photos/200/304'
+    },
+    {
+        name: 'Juliano Silva',
+        avatar: 'https://picsum.photos/200/305'
+    },
+    {
+      name: 'John Smith',
+      avatar: 'https://picsum.photos/200/302'
+    },
+    {
+      name: 'Pedro Perez',
+      avatar: 'https://picsum.photos/200/306'
+    },
+]
+
+export default users
