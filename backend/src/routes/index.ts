ebfde8dc263789cf2279f50a86060067e2039201
Gischenato <gischenato@gmail.com>
2022-12-05 00:09:39 -0300

    app routes

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { postRoutes } from "./post.routes";
+import { userRoutes } from "./user.routes";
+
+const routes = Router()
+
+routes.use('/users', userRoutes)
+routes.use('/posts', postRoutes)
+
+export default routes
