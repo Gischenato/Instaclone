f0a52c0d028cdb8f76131e801714bfa009fa5f67
Gischenato <gischenato@gmail.com>
2022-12-05 00:10:20 -0300

    aplication server

---
+++ backend/src/server.ts
@@ -0,0 +1,36 @@
+import 'express-async-errors'
+import express, { NextFunction, Request, Response } from 'express'
+import { AppError } from './errors/AppError'
+import routes from './routes'
+import { ZodError } from 'zod'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(routes)
+
+app.use((error:Error, request:Request, response:Response, next:NextFunction) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    if(error instanceof ZodError){
+        return response.status(400).json({
+            status: "error",
+            message: error.flatten()
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${error.message}`
+    })
+})
+
+app.listen(3333, () => {
+    console.log("Server is running on port 3333 🚀")
+})
