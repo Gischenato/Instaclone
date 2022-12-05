936d537f731cb9392dfe9ba5c053a7ed9f913518
Gischenato <gischenato@gmail.com>
2022-12-05 00:41:20 -0300

    getPosts routes

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import { CreatePostController } from '../modules/post/useCases/createPost/CreatePostController'
+import { GetAllPostsController } from '../modules/post/useCases/getAllPosts/GetAllPostsController'
+import { GetPostsByUsernameController } from '../modules/post/useCases/getPostsByUsername/GetPostsByUsernameController'
 
 const createPostController = new CreatePostController()
+const getAllPostsController = new GetAllPostsController()
+const getPostsByUsernameController = new GetPostsByUsernameController()
 
 const postRoutes = Router()
 
 postRoutes.post('/', createPostController.handle)
+postRoutes.get('/', getAllPostsController.handle)
+postRoutes.get('/user', getPostsByUsernameController.handle)
 
 export { postRoutes }
